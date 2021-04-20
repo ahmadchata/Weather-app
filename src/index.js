639eb74a2b8773b5f82c1f7040086adfeeddff70
Ahmad Chata <ahmadchata@gmail.com>
2021-04-20 14:06:59 +0100

    Add temperature to view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './style.css';
 
+const { countryCodeEmoji } = require('country-code-emoji');
 const locationName = document.getElementById('weatherName');
+const locationWeather = document.getElementById('weatherTemp');
+const locationFlag = document.getElementById('weatherFlag');
 
 const getWeather = async () => {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Bida&APPID=8c2cf7df09a32e8715f1725a828b777b', {mode: 'cors'});
+  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Bida&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b', {mode: 'cors'});
   const responseData = await response.json();
   locationName.textContent = responseData.name;
   locationName.classList.add('name');
+  locationWeather.textContent = Math.round(responseData.main.temp) + '°';
+  locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
   console.log(responseData);
 };
 getWeather();
