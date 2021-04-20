eed456dacaae8100eb42fbd995fdf8884ea9d758
Ahmad Chata <ahmadchata@gmail.com>
2021-04-20 14:30:06 +0100

    Add weather description

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ const { countryCodeEmoji } = require('country-code-emoji');
 const locationName = document.getElementById('weatherName');
 const locationWeather = document.getElementById('weatherTemp');
 const locationFlag = document.getElementById('weatherFlag');
+const locationdesc = document.querySelector('.description');
 
 const getWeather = async () => {
   const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Bida&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b', {mode: 'cors'});
   const responseData = await response.json();
   locationName.textContent = responseData.name;
   locationName.classList.add('name');
-  locationWeather.textContent = Math.round(responseData.main.temp) + '°';
+  locationWeather.textContent = Math.round(responseData.main.temp) + '°C';
   locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
+  locationdesc.textContent = responseData.weather[0].description;
   console.log(responseData);
 };
 getWeather();
+
+export default getWeather;
