7bb57a6490f9232d156b0fc795e3e5d0d27c5b49
Ahmad Chata <ahmadchata@gmail.com>
2021-04-20 16:38:44 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './style.css';
 
 const { countryCodeEmoji } = require('country-code-emoji');
+
 const locationName = document.getElementById('weatherName');
 const locationWeather = document.getElementById('weatherTemp');
 const locationFlag = document.getElementById('weatherFlag');
 const locationdesc = document.querySelector('.description');
 const myForm = document.querySelector('form');
-const button = document.getElementById('submit');
 
 const getWeather = async (location) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b`, {mode: 'cors'});
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b`, { mode: 'cors' });
   const responseData = await response.json();
   locationName.textContent = responseData.name;
   locationName.classList.add('name');
-  locationWeather.textContent = Math.round(responseData.main.temp) + '°C';
+  locationWeather.textContent = `${Math.round(responseData.main.temp)}°C`;
   locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
   locationdesc.textContent = responseData.weather[0].description;
   myForm.reset();
@@ -22,11 +22,11 @@ const getWeather = async (location) => {
 const fetchWeather = () => {
   const location = document.getElementById('citySearch').value;
   getWeather(location);
-}
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
   fetchWeather();
-}
+};
 
 myForm.addEventListener('submit', handleSubmit);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-    module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
