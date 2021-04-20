4ef3af95ffbb83e5d83639db3d0699dd39fd20d0
Ahmad Chata <ahmadchata@gmail.com>
2021-04-20 13:00:35 +0100

    Add search form

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const getWeather = async () => {
   const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Bida&APPID=8c2cf7df09a32e8715f1725a828b777b', {mode: 'cors'});
   const responseData = await response.json();
   locationName.textContent = responseData.name;
+  locationName.classList.add('name');
   console.log(responseData);
 };
 getWeather();
