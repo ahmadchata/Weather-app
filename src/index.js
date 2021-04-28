cb658f87e62e06fd891be27bd3f1c3b527d23f32
Ahmad Chata <ahmadchata@gmail.com>
2021-04-28 11:16:11 +0100

    Add toggle button for Celsius or Fahrenheit

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ const locationWeather = document.getElementById('weatherTemp');
 const locationFlag = document.getElementById('weatherFlag');
 const locationdesc = document.querySelector('.description');
 const myForm = document.querySelector('form');
+const tempButton = document.getElementById('tempConversion');
+var celsius = true;
 
 const getWeather = async (location) => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b`, { mode: 'cors' });
   const responseData = await response.json();
   locationName.textContent = responseData.name;
   locationName.classList.add('name');
-  locationWeather.textContent = `${Math.round(responseData.main.temp)}°C`;
+  locationWeather.textContent = `${Math.round(responseData.main.temp)}`;
   locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
   locationdesc.textContent = responseData.weather[0].description;
   myForm.reset();
@@ -29,4 +31,23 @@ const handleSubmit = (e) => {
   fetchWeather();
 };
 
+const convertTempFah = () => {
+  console.log(locationWeather.textContent);
+  const fah = (locationWeather.textContent * 9/5) + 32;
+  locationWeather.textContent = fah;
+  celsius = false;
+}
+
+const convertTempCelsius = () => {
+  console.log(locationWeather.textContent);
+  const cel = (locationWeather.textContent - 32) * 5/9;
+  locationWeather.textContent = cel;
+  celsius = true;
+}
+
+const toggleTemp = () => {
+  celsius ? convertTempFah() : convertTempCelsius();
+}
+
 myForm.addEventListener('submit', handleSubmit);
+tempButton.addEventListener('click', toggleTemp);
