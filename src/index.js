118432a4ef5e122fcdb1035e83225c3ac8c2b564
Ahmad Chata <ahmadchata@gmail.com>
2021-04-28 12:41:37 +0100

    Add error handling when a location is not found

--- src/index.js
+++ src/index.js
@@ -8,17 +8,21 @@ const locationFlag = document.getElementById('weatherFlag');
 const locationdesc = document.querySelector('.description');
 const myForm = document.querySelector('form');
 const tempButton = document.getElementById('tempConversion');
+const error = document.querySelector('.error');
 var celsius = true;
 
 const getWeather = async (location) => {
+  try {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b`, { mode: 'cors' });
-  const responseData = await response.json();
+  const responseData = await response.json()
   locationName.textContent = responseData.name;
   locationName.classList.add('name');
   locationWeather.textContent = `${Math.round(responseData.main.temp)}`;
   locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
   locationdesc.textContent = responseData.weather[0].description;
-  myForm.reset();
+  myForm.reset() } catch (e) {
+    error.textContent = 'Location not found';
+  }
 };
 
 const fetchWeather = () => {
