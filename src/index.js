027334384d64ab3727d966337ebbd742798c2138
Ahmad Chata <ahmadchata@gmail.com>
2021-04-28 12:57:38 +0100

    Remove span element from index

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
-import './style.css';
+import "./style.css";
 
-const { countryCodeEmoji } = require('country-code-emoji');
+const { countryCodeEmoji } = require("country-code-emoji");
 
-const locationName = document.getElementById('weatherName');
-const locationWeather = document.getElementById('weatherTemp');
-const locationFlag = document.getElementById('weatherFlag');
-const locationdesc = document.querySelector('.description');
-const myForm = document.querySelector('form');
-const tempButton = document.getElementById('tempConversion');
-const error = document.querySelector('.error');
+const locationName = document.getElementById("weatherName");
+const locationWeather = document.getElementById("weatherTemp");
+const locationFlag = document.getElementById("weatherFlag");
+const locationdesc = document.querySelector(".description");
+const myForm = document.querySelector("form");
+const tempButton = document.getElementById("tempConversion");
+const error = document.querySelector(".error");
 var celsius = true;
 
 const getWeather = async (location) => {
   try {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b`, { mode: 'cors' });
-  const responseData = await response.json()
-  locationName.textContent = responseData.name;
-  locationName.classList.add('name');
-  locationWeather.textContent = `${Math.round(responseData.main.temp)}`;
-  locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
-  locationdesc.textContent = responseData.weather[0].description;
-  myForm.reset() } catch (e) {
-    error.textContent = 'Location not found';
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=8c2cf7df09a32e8715f1725a828b777b`,
+      { mode: "cors" }
+    );
+    const responseData = await response.json();
+    locationName.textContent = responseData.name;
+    locationName.classList.add("name");
+    locationWeather.textContent = `${Math.round(responseData.main.temp)}`;
+    locationFlag.textContent = countryCodeEmoji(responseData.sys.country);
+    locationdesc.textContent = responseData.weather[0].description;
+    myForm.reset();
+  } catch (e) {
+    locationdesc.textContent = "Location not found";
   }
 };
 
 const fetchWeather = () => {
-  const location = document.getElementById('citySearch').value;
+  const location = document.getElementById("citySearch").value;
   getWeather(location);
 };
 
@@ -37,21 +41,21 @@ const handleSubmit = (e) => {
 
 const convertTempFah = () => {
   console.log(locationWeather.textContent);
-  const fah = (locationWeather.textContent * 9/5) + 32;
+  const fah = Math.round((locationWeather.textContent * 9) / 5 + 32);
   locationWeather.textContent = fah;
   celsius = false;
-}
+};
 
 const convertTempCelsius = () => {
   console.log(locationWeather.textContent);
-  const cel = (locationWeather.textContent - 32) * 5/9;
+  const cel = Math.round(((locationWeather.textContent - 32) * 5) / 9);
   locationWeather.textContent = cel;
   celsius = true;
-}
+};
 
 const toggleTemp = () => {
   celsius ? convertTempFah() : convertTempCelsius();
-}
+};
 
-myForm.addEventListener('submit', handleSubmit);
-tempButton.addEventListener('click', toggleTemp);
+myForm.addEventListener("submit", handleSubmit);
+tempButton.addEventListener("click", toggleTemp);
